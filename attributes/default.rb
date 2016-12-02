8de7926923f508855e54f825b885e85294915784
Danny Guinther <danny@zensight.co>
2016-12-02 09:18:10 -0500

    Allow specifying docker version through node attrs

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default["docker_service"]["install_method"] = "tarball"
+default["docker_service"]["version"] = "1.12.3"
+default["docker_service"]["checksum"] = "626601deb41d9706ac98da23f673af6c0d4631c4d194a677a9a1a07d7219fa0f"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,6 @@
 docker_service "default" do
   action [ :create, :start ]
+  install_method node["docker_service"]["install_method"]
+  version node["docker_service"]["version"]
+  checksum node["docker_service"]["checksum"]
 end
